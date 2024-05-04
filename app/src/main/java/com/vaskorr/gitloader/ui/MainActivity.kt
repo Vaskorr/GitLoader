47d3f2878ffe9464864dfc494f2adddb7a026ca0
Vaskorr <basil.suntsov@yandex.ru>
2024-05-05 02:15:37 +0300

    add splashscren

--- app/src/main/java/com/vaskorr/gitloader/ui/MainActivity.kt
+++ app/src/main/java/com/vaskorr/gitloader/ui/MainActivity.kt
@@ -5,13 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.vaskorr.gitloader.feature.search_repositories.internal.screen.search_screen.SearchScreen
 import com.vaskorr.gitloader.core.theme.GitLoaderTheme
+import com.vaskorr.gitloader.feature.search_repositories.internal.screen.search_screen.SearchScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
